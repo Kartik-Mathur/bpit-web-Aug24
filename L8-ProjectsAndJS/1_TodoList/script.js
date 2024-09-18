3ac890fa4068329c358f627c446da868bbc15cdf
Kartik <kartik.mathur10@gmail.com>
2024-09-18 15:47:54 +0530

    Todo Project Complete

--- L8-ProjectsAndJS/1_TodoList/script.js
+++ L8-ProjectsAndJS/1_TodoList/script.js
@@ -4,16 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskList = document.getElementById('task-list');
     const filterButtons = document.querySelectorAll('.filter-buttons button');
     const clearCompletedButton = document.getElementById('clear-completed');
+    const allTasksSelectBtn = document.getElementById('filter-all');
+    const activeTasksSelectBtn = document.getElementById('filter-active');
+    const completedTasksSelectBtn = document.getElementById('filter-completed');
 
     taskForm.addEventListener('submit', addTask);
     let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    function renderTask() {
+    function renderTask(newTasks = tasks) {
         // Clear the entire taskList
         taskList.innerHTML = '';
 
         // Add all the tasks present inside the tasks array to taskList
-        tasks.forEach((task, indx) => {
+        newTasks.forEach((task, indx) => {
             const li = document.createElement('li');
             li.innerHTML = `
             <span class="task-text">${task.text}</span>
@@ -39,21 +42,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Edit Task
-    function editTask(li,indx){
-        let span=li.firstElementChild;
+    function editTask(li, indx) {
+        let span = li.firstElementChild;
         // Merko span ki jagah ek input element daalna padega so that user can change the value of
         // text
         const input = document.createElement('input');
         input.type = 'text';
         input.value = li.firstElementChild.innerText;
-        li.replaceChild(input,span);
+        li.replaceChild(input, span);
         input.focus();
 
-        input.addEventListener('blur',(ev)=>{
-            let updatedTaskValue=input.value.trim();
+        input.addEventListener('blur', (ev) => {
+            let updatedTaskValue = input.value.trim();
             console.log(updatedTaskValue);
-            if(updatedTaskValue){
-                tasks[indx].text=updatedTaskValue;
+            if (updatedTaskValue) {
+                tasks[indx].text = updatedTaskValue;
             }
             saveTask();
             renderTask();
@@ -98,6 +101,55 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     clearCompletedButton.addEventListener('click', clearCompleted);
+
+
+
+
+
+    // Filter tasks functionality
+    document.querySelector('.filter-buttons').addEventListener('click', (ev) => {
+        // console.log(ev.target);
+        const id = ev.target.getAttribute('id');
+        const taskStatus = id.split('-').pop();
+        /*if (taskStatus === 'all') {
+            allTasksSelectBtn.classList.add('active');
+            completedTasksSelectBtn.classList.remove('active');
+            activeTasksSelectBtn.classList.remove('active');
+        }
+        else if (taskStatus === 'active') {
+            allTasksSelectBtn.classList.remove('active');
+            completedTasksSelectBtn.classList.remove('active');
+            activeTasksSelectBtn.classList.add('active');
+        }
+        else if (taskStatus === 'completed') {
+            allTasksSelectBtn.classList.remove('active');
+            completedTasksSelectBtn.classList.add('active');
+            activeTasksSelectBtn.classList.remove('active');
+        }*/
+        // console.log(filterButtons);
+        filterButtons.forEach((item) => {
+            const currentId = item.getAttribute('id');
+            const currentStatus = currentId.split('-').pop();
+            if (taskStatus === currentStatus) {
+                item.classList.add('active');
+            }
+            else {
+                item.classList.remove('active');
+            }
+        })
+
+        if (taskStatus === 'all') {
+            renderTask();
+        }
+        else if (taskStatus === 'active') {
+            let myTasks = tasks.filter(t => t.completed === false);
+            renderTask(myTasks);
+        }
+        else if (taskStatus === 'completed') {
+            let myTasks = tasks.filter(t => t.completed === true);
+            renderTask(myTasks);
+        }
+    })
 
     // Starting mei renderTask ka call hona jarrori  hai taaki,
     // localStorage ke tasks load  ho jaaye frontEnd par
