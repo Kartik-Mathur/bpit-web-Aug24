19ef8143f10ea7d2fa527b35392af427db993e8e
Kartik <kartik.mathur10@gmail.com>
2024-09-18 13:03:36 +0530

    RenderTask Function and AddTask Code

--- L8-ProjectsAndJS/1_TodoList/script.js
+++ L8-ProjectsAndJS/1_TodoList/script.js
@@ -5,13 +5,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const filterButtons = document.querySelectorAll('.filter-buttons button');
     const clearCompletedButton = document.getElementById('clear-completed');
 
-    taskForm.addEventListener('submit',addTask);
+    taskForm.addEventListener('submit', addTask);
     let tasks = [];
 
-    function addTask(e){
+    function renderTask() {
+        // Clear the entire taskList
+        taskList.innerHTML = '';
+
+        // Add all the tasks present inside the tasks array to taskList
+        tasks.forEach((task) => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <span class="task-text">${task.text}</span>
+            <div class="task-actions">
+                <button class="complete-btn">${task.completed ? 'Undo' : 'Complete'}</button>
+                <button class="edit-btn">Edit</button>
+                <button class="delete-btn">Delete</button>
+            </div>
+            `;
+
+            li.className = `task-item ${task.completed ? 'completed' : ''}`;
+
+            taskList.appendChild(li);
+        })
+    }
+
+    function addTask(e) {
         e.preventDefault(); // This will prevent the page from getting refreshed
         const task = taskInput.value.trim();
-        if(task){
+        if (task) {
             tasks.push({
                 text: task,
                 completed: false
