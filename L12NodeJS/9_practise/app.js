8ac46afad2a9d5e87851072de786bcc578088895
Kartik <kartik.mathur10@gmail.com>
2024-10-10 14:39:52 +0530

    Todos CRUD functions backend

--- L12NodeJS/9_practise/app.js
+++ L12NodeJS/9_practise/app.js
@@ -6,18 +6,36 @@ const PORT = 4444;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-let todos = [];
+let todos = ['dance', 'swim', 'sing'];
+
 // Will return the entire todos array
-// app.get('/todos',)
+app.get('/todos', (req, res) => {
+    res.send(todos);
+})
 
 // Will add an element to todos array
-// app.post('/todos',)
+app.post('/todos', (req, res) => {
+    const { name } = req.body;
+    todos.push(name);
+    res.send(todos);
+})
 
 // Will delete an element from todos array by name
-// app.get('/delete-todo',)
+app.get('/delete-todo', (req, res) => {
+    const { name } = req.query;
+    todos = todos.filter(todo => todo !== name);
+    res.send(todos);
+})
 
 // Will edit an element from todos array by name
-// app.get('/update-todo',)
+app.get('/update-todo', (req, res) => {
+    const { name, newName } = req.query;
+    let indx = todos.indexOf(name);
+    if (indx != -1) {
+        todos[indx] = newName;
+    }
+    res.send(todos);
+})
 
 app.listen(PORT, () => {
     console.log(`http://localhost:` + PORT);
