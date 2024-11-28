3ad83a5febe94c2d797377b393e093e1c1a94916
Kartik <kartik.mathur10@gmail.com>
2024-11-28 14:19:32 +0530

    DB-Populate

--- L18-Databases-sessions/routes/book-route.js
+++ L18-Databases-sessions/routes/book-route.js
@@ -1,14 +1,54 @@
 import path from 'path';
 import express from 'express';
+import insertBooks from '../utils/helper_functions/insertBooks.js';
+import Book from '../model/book.js';
 const router = express.Router();
 
-router.post('/add-books', (req, res, next) => {
-   
+router.post('/add-books', async (req, res, next) => {
+    const { name, books } = req.body;
+    if (!name || !books) {
+        return res.status(400).json({
+            msg: "Please provide both author name and books to add"
+        });
+    }
+
+    try {
+        let data = await insertBooks(name, books);
+        res.status(200).json({
+            msg: "Books addedd successfully",
+            data
+        })
+    } catch (error) {
+        reject({
+            msg: "Books couldnot be added",
+            error: error.message,
+            status: 500
+        })
+    }
+
 });
 
-router.get('/book-detail',(req,res,next)=>{
-    
-})
+router.get('/book-detail', async (req, res, next) => {
+    const { name } = req.query;
+    if(!name) return res.status(400).json({
+        msg:"Please provide name to search"
+    })
 
+    try {
+        let book = await Book.find({
+            name
+        }).populate('author_id').exec();
+        res.status(200).json({
+            msg:"Book fetched successfully",
+            data: book
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg:"Book fetching failed",
+            error:error.message
+        })
+    }
+
+});
 
 export default router;
--- L18-Databases-sessions/utils/helper_functions/insertBooks.js
+++ L18-Databases-sessions/utils/helper_functions/insertBooks.js
@@ -1,8 +1,40 @@
 import Book from "../../model/book.js";
 import Person from "../../model/person.js";
 
-export default function insertBooks(author_name, books) {
-    return new Promise((resolve, reject) => {
-        
+export default function insertBooks(name, books) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let author = await Person.findOne({
+                name
+            })
+
+            if (!author) {
+                return res.status(400).json({
+                    msg: "Author doesnot exist, try with existing author"
+                })
+            }
+            books = books.map((b) => {
+                return {
+                    ...b,
+                    author_id: author.id
+                }
+            })
+
+            let books_data = await Book.insertMany(books);
+
+            // console.log(books_data);
+            let newBook_ids = books_data.map(d => d._id);
+            // console.log(newBook_ids);
+            author.book_ids = [...newBook_ids, ...author.book_ids];
+            await author.save();
+            
+            resolve({ author, books: books_data });
+        } catch (error) {
+            reject({
+                msg: "Author couldnot be added",
+                error: error.message,
+                status: 500
+            })
+        }
     })
 }
